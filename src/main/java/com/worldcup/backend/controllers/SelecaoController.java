b9504b59de29134fa72a104b817a54004d9819c2
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-05-23 16:20:58 -0300

    feat: implementação do endpoint DELETE por ID no SelecaoController

--- src/main/java/com/worldcup/backend/controllers/SelecaoController.java
+++ src/main/java/com/worldcup/backend/controllers/SelecaoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +28,11 @@ public class SelecaoController {
     @GetMapping("{id}")
     public ResponseEntity<Selecao> getById(@PathVariable long id){
         return ResponseEntity.ok(service.findById(id));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable long id){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
