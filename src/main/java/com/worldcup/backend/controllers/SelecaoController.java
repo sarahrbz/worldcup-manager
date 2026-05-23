99f8d09ff736882fbaca844b3d80d8c4d56597e5
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-05-23 16:38:39 -0300

    feat: implementação do endpoint PUT no SelecaoController closes #5

--- src/main/java/com/worldcup/backend/controllers/SelecaoController.java
+++ src/main/java/com/worldcup/backend/controllers/SelecaoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,4 +51,11 @@ public class SelecaoController {
                         .buildAndExpand(s.getId())
                         .toUri();
         return ResponseEntity.created(location).body(s);
-    }}
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> update(@RequestBody Selecao selecao, @PathVariable Long id){
+        service.update(selecao, id);
+        return ResponseEntity.noContent().build();
+    }
+}
