7671e8dcf0faadf1850b5a0be0f4ae9fbe285921
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-05-23 15:47:21 -0300

    feat: implementação do método GET no SelecaoService

--- src/main/java/com/worldcup/backend/services/SelecaoService.java
+++ src/main/java/com/worldcup/backend/services/SelecaoService.java
@@ -1,8 +1,20 @@
 package com.worldcup.backend.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.worldcup.backend.entities.Selecao;
+import com.worldcup.backend.repositories.SelecaoRepository;
 
 @Service
 public class SelecaoService {
+    @Autowired
+    private SelecaoRepository repository;
+
+    public List<Selecao> findAll() {
+        return repository.findAll();
+    }
     
 }
