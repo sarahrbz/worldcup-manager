7ceab76bafffbb7fb66bef00e14aaa00188afef3
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-05-23 16:08:52 -0300

    feat: implementação do endpoint GET por ID no SelecaoController

--- src/main/java/com/worldcup/backend/controllers/SelecaoController.java
+++ src/main/java/com/worldcup/backend/controllers/SelecaoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class SelecaoController {
     @GetMapping
     public ResponseEntity<List<Selecao>> getAll(){
         return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Selecao> getById(@PathVariable long id){
+        return ResponseEntity.ok(service.findById(id));
     }
 }
