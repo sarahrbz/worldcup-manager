0bd493dbfb577560ce30f0e036df962390a7f32c
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-05-23 16:28:28 -0300

    feat: implementação do endpoint POST no SelecaoController

--- src/main/java/com/worldcup/backend/controllers/SelecaoController.java
+++ src/main/java/com/worldcup/backend/controllers/SelecaoController.java
@@ -1,5 +1,6 @@
 package com.worldcup.backend.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.worldcup.backend.entities.Selecao;
 import com.worldcup.backend.services.SelecaoService;
@@ -35,4 +39,15 @@ public class SelecaoController {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
     }
-}
+
+    @PostMapping
+    public ResponseEntity<Selecao> save(@RequestBody Selecao selecao){
+        Selecao s = service.save(selecao);
+
+        URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(s.getId())
+                        .toUri();
+        return ResponseEntity.created(location).body(s);
+    }}
