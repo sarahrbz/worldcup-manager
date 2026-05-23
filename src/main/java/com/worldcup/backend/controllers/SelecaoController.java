fb5793c675f386bd85d7d58a2dacc38e184a5a56
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-05-23 15:56:27 -0300

    feat: implementação do endpoint GET no SelecaoController

--- src/main/java/com/worldcup/backend/controllers/SelecaoController.java
+++ src/main/java/com/worldcup/backend/controllers/SelecaoController.java
@@ -1,8 +1,25 @@
 package com.worldcup.backend.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.worldcup.backend.entities.Selecao;
+import com.worldcup.backend.services.SelecaoService;
+
 @RestController
+@RequestMapping("/selecoes")
 public class SelecaoController {
+
+    @Autowired
+    private SelecaoService service;
     
+    @GetMapping
+    public ResponseEntity<List<Selecao>> getAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
 }
