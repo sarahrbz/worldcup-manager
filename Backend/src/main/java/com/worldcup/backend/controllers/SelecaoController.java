c4dce9af5231d561d1a341c1aa69730f5c8c1430
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-05-29 20:37:08 -0300

    feat: integração entre frontend e backend de seleções

--- Backend/src/main/java/com/worldcup/backend/controllers/SelecaoController.java
+++ Backend/src/main/java/com/worldcup/backend/controllers/SelecaoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/selecoes")
+@CrossOrigin
 public class SelecaoController {
 
     @Autowired
