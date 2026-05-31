0c701be727a354e642dc59881522bc31f0a4b800
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-05-31 20:15:54 -0300

    feat: criação do JogadorController e JogadorService - closes #13 closes #14

---
+++ Backend/src/main/java/com/worldcup/backend/services/JogadorService.java
@@ -0,0 +1,8 @@
+package com.worldcup.backend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JogadorService {
+    
+}
--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -6,3 +6,36 @@ VALUES ('Argentina', 'Lionel Scaloni', 'C', 1, 3);
 
 INSERT INTO TBL_SELECOES (name, coach, team_group, fifa_ranking, number_of_titles)
 VALUES ('França', 'Didier Deschamps', 'D', 2, 2);
+
+-- BRASIL
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Vinicius Junior', 7, 'Atacante', 25, 'Real Madrid', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Alisson Becker', 1, 'Goleiro', 33, 'Liverpool', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Marquinhos', 4, 'Zagueiro', 31, 'PSG', 1);
+
+-- ARGENTINA
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Lionel Messi', 10, 'Atacante', 39, 'Inter Miami', 2);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Julian Alvarez', 9, 'Atacante', 26, 'Atletico Madrid', 2);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Emiliano Martinez', 23, 'Goleiro', 34, 'Aston Villa', 2);
+
+-- FRANÇA
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Kylian Mbappe', 10, 'Atacante', 27, 'Real Madrid', 3);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Mike Maignan', 16, 'Goleiro', 31, 'Milan', 3);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Aurelien Tchouameni', 8, 'Meio-Campo', 26, 'Real Madrid', 3);
