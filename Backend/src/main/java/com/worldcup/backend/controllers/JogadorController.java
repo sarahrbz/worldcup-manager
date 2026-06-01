ab63d638686f0216c43dc59129e2c52bb6224aee
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-05-31 21:23:15 -0300

    feat: implementação do CRUD completo de jogador closes #15

--- Backend/src/main/java/com/worldcup/backend/controllers/JogadorController.java
+++ Backend/src/main/java/com/worldcup/backend/controllers/JogadorController.java
@@ -1,10 +1,64 @@
 package com.worldcup.backend.controllers;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.worldcup.backend.dtos.JogadorRequest;
+import com.worldcup.backend.dtos.JogadorResponse;
+import com.worldcup.backend.services.JogadorService;
 
 @RestController
 @RequestMapping("/jogadores")
 public class JogadorController {
-    
+
+    @Autowired
+    private JogadorService service;
+
+    @GetMapping
+    public ResponseEntity<List<JogadorResponse>> getAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JogadorResponse> getById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<JogadorResponse> save(@RequestBody JogadorRequest jogador){
+        JogadorResponse j = service.save(jogador);
+
+        URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(j.id())
+                        .toUri();
+        return ResponseEntity.created(location).body(j);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody JogadorRequest jogador, @PathVariable Long id){
+        service.update(jogador, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
--- Backend/src/main/java/com/worldcup/backend/services/JogadorService.java
+++ Backend/src/main/java/com/worldcup/backend/services/JogadorService.java
@@ -1,8 +1,75 @@
 package com.worldcup.backend.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.worldcup.backend.dtos.JogadorRequest;
+import com.worldcup.backend.dtos.JogadorResponse;
+import com.worldcup.backend.entities.Jogador;
+import com.worldcup.backend.entities.Selecao;
+import com.worldcup.backend.mappers.JogadorMapper;
+import com.worldcup.backend.repositories.JogadorRepository;
+import com.worldcup.backend.repositories.SelecaoRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class JogadorService {
-    
+
+    @Autowired
+    private JogadorRepository repository;
+
+    @Autowired
+    private SelecaoRepository selecaoRepository;
+
+    public List<JogadorResponse> findAll(){
+        return repository.findAll()
+            .stream()
+            .map(JogadorMapper::toDTO)
+            .toList();
+    }
+
+    public JogadorResponse findById(Long id){
+        return repository.findById(id)
+            .map(JogadorMapper::toDTO)
+            .orElseThrow(() -> new EntityNotFoundException("Jogador não encontrado"));
+    }
+
+    public JogadorResponse save(JogadorRequest jogador){
+        Selecao selecao = selecaoRepository.findById(jogador.selecaoId()) // Busca a seleção pelo ID fornecido no request
+            .orElseThrow(() -> new EntityNotFoundException("Seleção não encontrada"));
+
+        Jogador j = repository.save(JogadorMapper.toEntity(jogador));
+        j.setSelecao(selecao); // Associa o jogador à seleção
+
+        return JogadorMapper.toDTO(j);
+    }
+
+    public void update(JogadorRequest jogador, Long id){
+        Jogador j = repository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Jogador não encontrado"));
+
+        j.setName(jogador.name());
+        j.setNumber(jogador.number());
+        j.setPosition(jogador.position());
+        j.setAge(jogador.age());
+        j.setClub(jogador.club());
+        
+        Selecao selecao = selecaoRepository.findById(jogador.selecaoId())
+                .orElseThrow(() -> new EntityNotFoundException("Seleção não encontrada"));
+
+    j.setSelecao(selecao);
+        repository.save(j);
+
+    }
+
+    public void delete(Long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Jogador não cadastrado");
+        }
+    }
 }
