728a1e329c0867b7500b7d528c40f505a80d5e18
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-06-04 15:40:58 -0300

    feat: implementação da listagem de jogadores closes #20

--- frontend/src/app/components/jogador-list/jogador-list.ts
+++ frontend/src/app/components/jogador-list/jogador-list.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Jogador } from '../../models/jogador';
+import { JogadorService } from '../../services/jogador-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-jogador-list',
@@ -6,6 +9,32 @@ import { Component } from '@angular/core';
   templateUrl: './jogador-list.html',
   styleUrl: './jogador-list.css',
 })
-export class JogadorList {
+export class JogadorList implements OnInit {
+
+  jogadores = signal<Jogador[]>([]);
+
+  constructor(private jogadorService: JogadorService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.jogadorService.getAllJogadores().subscribe({
+      next: json => this.jogadores.set(json)
+    });
+  }
+
+  update(jogador: Jogador) {
+    this.router.navigate(['/jogador-form', jogador.id]); // Navega para o formulário de edição passando o ID do jogador
+  }
+
+  delete(jogador: Jogador) {
+    this.jogadorService.delete(jogador).subscribe({
+      next: () => {
+        this.jogadores.update(jogadores => jogadores.filter(j => j.id !== jogador.id));
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Não foi possível excluir o jogador.');
+      }
+    });
+  }
 
 }
