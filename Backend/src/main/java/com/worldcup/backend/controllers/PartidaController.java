ceb62311407541c6fad16dee9e2db7b1b9e6c823
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-06-06 20:09:22 -0300

    feat: implementação do CRUD de Partida closes #25

--- Backend/src/main/java/com/worldcup/backend/controllers/PartidaController.java
+++ Backend/src/main/java/com/worldcup/backend/controllers/PartidaController.java
@@ -1,10 +1,64 @@
 package com.worldcup.backend.controllers;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.worldcup.backend.dtos.PartidaRequest;
+import com.worldcup.backend.dtos.PartidaResponse;
+import com.worldcup.backend.services.PartidaService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/partidas")
 public class PartidaController {
     
+    @Autowired
+    private PartidaService service;
+
+    @GetMapping
+    public ResponseEntity<List<PartidaResponse>> getAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PartidaResponse> getById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<PartidaResponse> save(@Valid @RequestBody PartidaRequest partida) {
+        PartidaResponse p = service.save(partida);
+
+        URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(p.id())
+                        .toUri();
+        return ResponseEntity.created(location).body(p);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody PartidaRequest partida, @PathVariable Long id){
+        service.update(partida, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- Backend/src/main/java/com/worldcup/backend/services/PartidaService.java
+++ Backend/src/main/java/com/worldcup/backend/services/PartidaService.java
@@ -1,8 +1,93 @@
 package com.worldcup.backend.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.worldcup.backend.dtos.PartidaRequest;
+import com.worldcup.backend.dtos.PartidaResponse;
+import com.worldcup.backend.entities.Partida;
+import com.worldcup.backend.entities.Selecao;
+import com.worldcup.backend.mappers.PartidaMapper;
+import com.worldcup.backend.repositories.PartidaRepository;
+import com.worldcup.backend.repositories.SelecaoRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class PartidaService {
+
+    @Autowired
+    private PartidaRepository repository;
+
+    @Autowired
+    private SelecaoRepository selecaoRepository;
+
+    public List<PartidaResponse> findAll(){
+        return repository.findAll()
+            .stream()
+            .map(PartidaMapper::toDTO)
+            .toList();
+    }
+
+    public PartidaResponse findById(Long id){
+        return repository.findById(id)
+            .map(PartidaMapper::toDTO)
+            .orElseThrow(() -> new EntityNotFoundException("Partida não encontrada"));
+    }
+
+    private void validarSelecoes(PartidaRequest partida) {
+        if(partida.mandanteId().equals(partida.visitanteId())){
+        throw new IllegalArgumentException(
+            "Uma seleção não pode jogar contra ela mesma"
+        );
+    }
+    }
+
+    public PartidaResponse save(PartidaRequest partida){
+        validarSelecoes(partida);
+        Selecao mandante = selecaoRepository.findById(partida.mandanteId()) // Busca a seleção mandante pelo ID fornecido no request
+            .orElseThrow(() -> new EntityNotFoundException("Seleção mandante não encontrada"));
+
+        Selecao visitante = selecaoRepository.findById(partida.visitanteId()) // Busca a seleção visitante pelo ID fornecido no request
+            .orElseThrow(() -> new EntityNotFoundException("Seleção visitante não encontrada"));
+
+        Partida p = repository.save(PartidaMapper.toEntity(partida));
+        p.setMandante(mandante); // Associa a seleção mandante à partida
+        p.setVisitante(visitante); // Associa a seleção visitante à partida
+        return PartidaMapper.toDTO(p);
+    }
+
+    public void update(PartidaRequest partida, Long id){
+        validarSelecoes(partida);
+        Partida p = repository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Partida não encontrada"));
+
+        p.setData(partida.data());
+        p.setEstadio(partida.estadio());
+        p.setGolsMandante(partida.golsMandante());
+        p.setGolsVisitante(partida.golsVisitante());
+        
+        Selecao mandante = selecaoRepository.findById(partida.mandanteId()) // Busca a seleção mandante pelo ID fornecido no request
+            .orElseThrow(() -> new EntityNotFoundException("Seleção mandante não encontrada"));
+
+        Selecao visitante = selecaoRepository.findById(partida.visitanteId()) // Busca a seleção visitante pelo ID fornecido no request
+            .orElseThrow(() -> new EntityNotFoundException("Seleção visitante não encontrada"));
+
+        p.setMandante(mandante); // Associa a seleção mandante à partida
+        p.setVisitante(visitante); // Associa a seleção visitante à partida
+
+        repository.save(p);
+    }
+
+    public void delete(Long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Partida não cadastrada");
+        }
+    }
     
+
 }
