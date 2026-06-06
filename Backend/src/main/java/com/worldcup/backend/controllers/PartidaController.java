cbc9ca716c263fd143e5e227644eb6092372d5ec
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-06-06 19:31:28 -0300

    feat: criação do service e controller de Partida closes #23 closes #24

---
+++ Backend/src/main/java/com/worldcup/backend/controllers/PartidaController.java
@@ -0,0 +1,10 @@
+package com.worldcup.backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/partidas")
+public class PartidaController {
+    
+}
