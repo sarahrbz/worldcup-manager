f08a14b45bd7d7fdf9a156283589b3bd6a366ec5
Sarah Ribeiro de Souza <sarah436ben@gmail.com>
2026-06-14 16:17:17 -0300

    feat: implementa listagem de partidas closes #30

--- frontend/src/app/components/partida-list/partida-list.ts
+++ frontend/src/app/components/partida-list/partida-list.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Partida } from '../../models/partida';
+import { PartidaService } from '../../services/partida-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-partida-list',
@@ -6,6 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './partida-list.html',
   styleUrl: './partida-list.css',
 })
-export class PartidaList {
+export class PartidaList implements OnInit {
+
+  partidas = signal<Partida[]>([]);
+
+  constructor(private partidaService: PartidaService, private router: Router) {}
+  ngOnInit(): void {
+    this.partidaService.getAllPartidas().subscribe({
+      next: json => this.partidas.set(json)
+    });
+  }
+
+  update(partida: Partida) {
+    this.router.navigate(['/partida-form', partida.id]); // Navega para o formulário de edição passando o ID da partida
+  }
+
+  delete(partida: Partida) {
+    this.partidaService.delete(partida).subscribe({
+      next: () => {
+        this.partidas.update(partidas => partidas.filter(p => p.id !== partida.id));
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Não foi possível excluir a partida.');
+      }
+    });
+  }
 
 }
