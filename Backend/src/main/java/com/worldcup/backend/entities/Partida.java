901a15884abc13ba5114f6e021c62e2a9af40fe0
Sarah Ribeiro de Souza <sarahrsz162@gmail.com>
2026-06-15 17:04:16 -0300

    chore: adiciona dados iniciais para testes
    - adicionadas novas seleções
    - adicionados jogadores para múltiplas seleções
    - adicionadas partidas para testes
    - ampliada massa de dados para validação da paginação

--- Backend/src/main/java/com/worldcup/backend/entities/Partida.java
+++ Backend/src/main/java/com/worldcup/backend/entities/Partida.java
@@ -11,7 +11,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name= "TBL_PARTIDA")
+@Table(name= "TBL_PARTIDAS")
 public class Partida implements Serializable {
     
     @Id
--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -7,6 +7,18 @@ VALUES ('Argentina', 'Lionel Scaloni', 'C', 1, 3);
 INSERT INTO TBL_SELECOES (name, coach, team_group, fifa_ranking, number_of_titles)
 VALUES ('França', 'Didier Deschamps', 'D', 2, 2);
 
+INSERT INTO TBL_SELECOES (name, coach, team_group, fifa_ranking, number_of_titles)
+VALUES ('Alemanha', 'Julian Nagelsmann', 'A', 10, 4);
+
+INSERT INTO TBL_SELECOES (name, coach, team_group, fifa_ranking, number_of_titles)
+VALUES ('Espanha', 'Luis de la Fuente', 'B', 3, 1);
+
+INSERT INTO TBL_SELECOES (name, coach, team_group, fifa_ranking, number_of_titles)
+VALUES ('Portugal', 'Roberto Martinez', 'F', 6, 0);
+
+INSERT INTO TBL_SELECOES (name, coach, team_group, fifa_ranking, number_of_titles)
+VALUES ('Inglaterra', 'Thomas Tuchel', 'E', 4, 1);
+
 -- BRASIL
 
 INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
@@ -17,6 +29,57 @@ VALUES ('Alisson Becker', 1, 'Goleiro', 33, 'Liverpool', 1);
 
 INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
 VALUES ('Marquinhos', 4, 'Zagueiro', 31, 'PSG', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Rodrygo', 11, 'Atacante', 25, 'Real Madrid', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Raphinha', 10, 'Atacante', 30, 'Barcelona', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Bruno Guimaraes', 8, 'Meio-Campo', 29, 'Newcastle United', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Casemiro', 5, 'Meio-Campo', 34, 'Manchester United', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Lucas Paqueta', 7, 'Meio-Campo', 29, 'West Ham', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Gabriel Magalhaes', 3, 'Zagueiro', 29, 'Arsenal', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Eder Militao', 14, 'Zagueiro', 28, 'Real Madrid', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Danilo', 2, 'Lateral', 35, 'Flamengo', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Guilherme Arana', 6, 'Lateral', 29, 'Atletico Mineiro', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Endrick', 9, 'Atacante', 20, 'Real Madrid', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Savinho', 21, 'Atacante', 22, 'Manchester City', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Bento', 12, 'Goleiro', 27, 'Al Nassr', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Gabriel Martinelli', 18, 'Atacante', 25, 'Arsenal', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Joao Gomes', 15, 'Meio-Campo', 25, 'Wolverhampton', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Bremer', 13, 'Zagueiro', 29, 'Juventus', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Yan Couto', 22, 'Lateral', 24, 'Borussia Dortmund', 1);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Andre', 17, 'Meio-Campo', 25, 'Wolverhampton', 1);
 
 -- ARGENTINA
 
@@ -39,3 +102,119 @@ VALUES ('Mike Maignan', 16, 'Goleiro', 31, 'Milan', 3);
 
 INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
 VALUES ('Aurelien Tchouameni', 8, 'Meio-Campo', 26, 'Real Madrid', 3);
+
+-- ALEMANHA
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Jamal Musiala', 10, 'Meio-Campo', 23, 'Bayern Munich', 4);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Antonio Rudiger', 2, 'Zagueiro', 33, 'Real Madrid', 4);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Marc-Andre ter Stegen', 1, 'Goleiro', 34, 'Barcelona', 4);
+
+-- ESPANHA
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Lamine Yamal', 19, 'Atacante', 19, 'Barcelona', 5);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Pedri', 8, 'Meio-Campo', 24, 'Barcelona', 5);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Unai Simon', 1, 'Goleiro', 29, 'Athletic Bilbao', 5);
+
+--PORTUGAL
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Cristiano Ronaldo', 7, 'Atacante', 41, 'Al Nassr', 6);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Bruno Fernandes', 8, 'Meio-Campo', 32, 'Manchester United', 6);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Diogo Costa', 1, 'Goleiro', 27, 'Porto', 6);
+
+-- INGLATERRA
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Harry Kane', 9, 'Atacante', 33, 'Bayern Munich', 7);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Jude Bellingham', 10, 'Meio-Campo', 23, 'Real Madrid', 7);
+
+INSERT INTO TBL_JOGADORES (name, number, position, age, club, selecao_id)
+VALUES ('Jordan Pickford', 1, 'Goleiro', 33, 'Everton', 7);
+
+-- PARTIDAS
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-10', 'Maracanã', 1, 2, 2, 1);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-11', 'Allianz Arena', 4, 5, 1, 1);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-12', 'Parc des Princes', 3, 6, 3, 2);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-13', 'Wembley', 7, 1, 0, 2);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-14', 'Monumental', 2, 3, 2, 2);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-15', 'Santiago Bernabeu', 5, 6, 3, 1);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-16', 'Allianz Arena', 4, 7, 2, 2);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-17', 'Maracana', 1, 5, 1, 0);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-18', 'Wembley', 7, 2, 1, 3);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-19', 'Parc des Princes', 3, 4, 2, 1);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-20', 'Estadio da Luz', 6, 1, 0, 2);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-21', 'Camp Nou', 5, 3, 2, 2);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-22', 'Monumental', 2, 4, 1, 0);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-23', 'Old Trafford', 7, 6, 3, 2);
+
+INSERT INTO TBL_PARTIDAS
+(data, estadio, mandante_id, visitante_id, gols_mandante, gols_visitante)
+VALUES
+('2026-06-24', 'Maracana', 1, 3, 4, 1);
